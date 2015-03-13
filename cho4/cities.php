84637d105dc6a572074f215621a905ab1bba3660
jeremyschmidt <jeremy.schmidt@cpnv.ch>
2015-03-13 09:15:46 +0100

    salut

--- cho4/cities.php
+++ cho4/cities.php
@@ -1,14 +1,44 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>test</title>
+</head>
+<body>
 
 
 <?php
 
-$dbh = new Mysqli ('localhost', 'cpnv', 'cpnv1234', 'world');
+$dbh = new mysqli ('localhost', 'cpnv', 'cpnv1234', 'world');
 
  if ($dbh->connect_error) {
-    echo "Problème de connexion à la base de données." . mysql_error();
-    exit;
+    echo "Problème de connexion à la base de données." ;
+    
 }
 
 
+if (!$result = $dbh->query("SELECT ID, Name, Countrycode, 
+    District from City where CountryCode = 'CHE' order by Name")) {
+        
+    die("{$dbh->errno} : {$dbh->error}");
+        
+}
 
+while($row = $result->fetch_assoc())
+    
+    {
+  echo $row['ID'] . ' ';
+  echo $row['Name'] . ' ';
+  echo $row['Countrycode'] . ' ';
+  echo $row['District'] . ' ';
+   echo "<a href=\"edit.php\">LIen vers la prochaine page</a>" . '</br>';
+    }
+    
+    {
+    $nbr = $result->num_rows;
+    echo "Il y a $nbr ligne(s)";
+}
 ?>
+
+
+</body>
+</html>
---
+++ cho4/edit.php
@@ -0,0 +1,19 @@
+ <html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>test</title>
+</head>
+<body>
+    
+    
+    
+  <h1>La page est en cours de maintenance! Nous faisons notre 
+  possible pour la remettre en ligne au plus vite!</h1></br>
+  <h2>Merci de votre compréhension.</h2>  </br>
+  <h3>La direction</h3>
+    
+    
+    
+
+</body>
+</html>
