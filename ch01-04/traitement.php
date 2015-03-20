0dad2bfd8a7837e69387d6f0e6055d6269dda4e6
jeremyschmidt <jeremy.schmidt@cpnv.ch>
2015-03-20 15:31:21 +0100

    Le test PHP

---
+++ ch01-04/traitement.php
@@ -0,0 +1,13 @@
+<?php
+
+$sexe = $_POST['M'];
+
+if ($sexe = M) {
+    echo "Bienvenue M.Truc, votre adresse courriel est : truc@muche.com";
+}
+else {
+    echo "Bienvenue Mme. Bidule, votre adresse courriel est : bidule@gmail.com";
+}
+
+
+?>
